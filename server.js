ae5b8115a30e658d65af344508e50872f438329e
Ruhama Yared <ruhamayared1@gmail.com>
2022-11-13 20:01:49 -0500

    starting innovation time

---
+++ server.js
@@ -0,0 +1,47 @@
+require("dotenv").config()
+const express = require('express')
+const budget = require("./models/budget.js")
+const methodOverride = require("method-override")
+
+const app = express()
+
+
+//Middleware
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
+
+
+// INDEX ROUTE - GET
+app.get('/', (req, res) => {
+    res.render("index.ejs", {
+        allBudget:budget
+    } )
+})
+
+// NEW ROUTE - GET
+app.get('/budget/new', (req, res) => {
+    res.render("new.ejs")
+})
+
+// Create Route - POST
+app.post('/budget', (req, res) => {
+    budget.push(req.body)
+    
+    res.redirect("/budget")
+})
+
+// SHOW ROUTE - GET
+app.get('/budget/:index', (req, res) => {
+    // res.render(template, data)
+    res.render('show.ejs', {
+        item: budget[req.params.index],
+        index: req.params.index
+    })
+})
+
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
